4d893693fdbf6ff25b34edb298d26b61896089ea
Manish <manishkumar0156@gmail.com>
2022-02-17 21:08:34 +0530

    [Manish] Add: added password validation

--- UserRegistration/Registration.cs
+++ UserRegistration/Registration.cs
@@ -14,9 +14,9 @@ namespace UserRegistration
             Regex reg = new Regex("^[A-Z]{1}[a-z]{3,12}$");
 
             Console.WriteLine("Enter FirstName");
-            string fname=Console.ReadLine();    
+            string fname = Console.ReadLine();
 
-            if(reg.IsMatch(fname))
+            if (reg.IsMatch(fname))
             {
                 Console.WriteLine("Your Name is: " + fname);
             }
@@ -43,14 +43,14 @@ namespace UserRegistration
 
             //UseCase 3
 
-            string regex1 ="^[a-zA-Z0-9]{3,}[.][a-zA-Z0-9]{3,}[@][a-zA-Z0-9]{2,}[.][a-zA-Z]{2,}[.][a-zA-Z]{2}?$";
+            string regex1 = "^[a-zA-Z0-9]{3,}[.][a-zA-Z0-9]{3,}[@][a-zA-Z0-9]{2,}[.][a-zA-Z]{2,}[.][a-zA-Z]{2}?$";
             string regex2 = "^[A-Za-z0-9]{3,}[@][A-za-z]{3,}[.][A-Za-z]{3}$";
 
             Regex exp1 = new Regex(regex1);
             Regex exp2 = new Regex(regex2);
 
             Console.WriteLine("Enter Email id");
-            string email=Console.ReadLine();
+            string email = Console.ReadLine();
             if (exp1.IsMatch(email) || exp2.IsMatch(email))
             {
                 Console.WriteLine("your Email id is :" + email);
@@ -74,6 +74,22 @@ namespace UserRegistration
             else
             {
                 Console.WriteLine("Enter valid Mobile Number!");
+            }
+
+            //UseCase 5
+
+
+            Regex regex4 = new Regex("^[a-zA-Z0-9]{8,}$");
+            Console.WriteLine("Enter password minimum 8 characters");
+            string password = Console.ReadLine();
+
+            if (regex4.IsMatch(password))
+            {
+                Console.WriteLine("Your password is :" + password);
+            }
+            else
+            {
+                Console.WriteLine("Enter valid Password");
             }
         }
     }
