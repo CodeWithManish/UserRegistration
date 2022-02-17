7036dbf4027a4aacb8d60215b8b809ba2662d24f
Manish <manishkumar0156@gmail.com>
2022-02-17 16:49:43 +0530

    [Manish] Add: Validating lastName using Regular Expression

--- UserRegistration/Registration.cs
+++ UserRegistration/Registration.cs
@@ -11,7 +11,7 @@ namespace UserRegistration
     {
         public void UserData()
         {
-            Regex reg = new Regex("^[A-Z]{1}[a-z]{2,12}$");
+            Regex reg = new Regex("^[A-Z]{1}[a-z]{3,12}$");
 
             Console.WriteLine("Enter FirstName");
             string fname=Console.ReadLine();    
@@ -25,7 +25,21 @@ namespace UserRegistration
                 Console.WriteLine("Please Enter valid Name!");
             }
 
+            //UseCase 2
 
+            Regex reg1 = new Regex("^[A-Z]{1}[a-z]{3,12}$");
+
+            Console.WriteLine("Enter LastName");
+            string lname = Console.ReadLine();
+
+            if (reg1.IsMatch(lname))
+            {
+                Console.WriteLine("Your Name is: " + lname);
+            }
+            else
+            {
+                Console.WriteLine("Please Enter valid Name!");
+            }
         }
     }
 }
