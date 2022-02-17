8459d2d44fb7cc73f59b00e4f6431d5f2cd17580
Manish <manishkumar0156@gmail.com>
2022-02-17 22:38:13 +0530

    [Manish] Add: Added Numeric number in password

--- UserRegistration/Registration.cs
+++ UserRegistration/Registration.cs
@@ -104,6 +104,23 @@ namespace UserRegistration
             else
             {
                 Console.WriteLine("Enter valid Password");
+
+
+            }
+
+            //
+            Regex pttrn = new Regex("^(?=.*[a-zA-Z])(?=.*[0-9]).+$");
+            Console.WriteLine("Enter password atleast one Numeric Number");
+            string pass = Console.ReadLine();
+            if (pttrn.IsMatch(pass))
+            {
+                Console.WriteLine("Your password is :" + pass);
+            }
+            else
+            {
+                Console.WriteLine("Enter valid Password");
+
+
             }
         }
     }
